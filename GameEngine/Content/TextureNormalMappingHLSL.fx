4e5352df98758148af25435c7e18c11f952b6fcf
DaniilLeshchenko <158283079+DaniilLeshchenko@users.noreply.github.com>
2025-03-27 16:37:01 +0000

    CustomEffectModel added, but not working

---
+++ GameEngine/Content/TextureNormalMappingHLSL.fx
@@ -0,0 +1,77 @@
+﻿bool TextureEnabled = false;
+
+texture Texture;
+
+float3 DiffuseColor = float3(1, 1, 1);
+float3 AmbientColor = float3(0.3f, 0.3f, 0.3f);
+
+float3 LightDirection = float3(1, 1, 1);
+float3 LightColor = float3(1, 1, 1);
+
+sampler TextureSampler = sampler_state
+{
+    Texture = <Texture>;
+};
+
+
+float4x4 World;
+float4x4 View;
+float4x4 Projection;
+
+struct VertexShaderInput
+{
+    float4 Position : SV_Position0;
+    float2 UV : TEXCOORD0;
+    float3 Normal : NORMAL0;
+};
+
+struct VertexShaderOutput
+{
+    float4 Position : SV_Position0;
+    float2 UV : TEXCOORD0;
+    float3 Normal : TEXCOORD1;
+};
+
+VertexShaderOutput VertexShaderFunction(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+    
+    float4 worldPosition = mul(input.Position, World);
+    float4 viewPosition = mul(worldPosition, View);
+    
+    output.Position = mul(viewPosition, Projection);
+    output.UV = input.UV;
+    output.Normal = normalize(mul(input.Normal, World));
+
+    return output;
+}
+
+float4 PixelShaderFunction(VertexShaderOutput input) : COLOR0
+{
+    float3 color = DiffuseColor;
+
+    if (TextureEnabled)
+    {
+        color *= tex2D(TextureSampler, input.UV).rgb;
+    }
+
+    float3 normal = normalize(input.Normal);
+    float3 lightDir = normalize(LightDirection);
+
+    float lambert = saturate(dot(-lightDir, normal));
+    float3 lighting = AmbientColor + (lambert * LightColor);
+
+    float3 finalColor = saturate(color * lighting);
+
+    return float4(finalColor, 1);
+}
+
+technique BasicTechnique
+{
+    pass Pass1
+    {
+        VertexShader = compile vs_4_0 VertexShaderFunction();
+        PixelShader = compile ps_4_0 PixelShaderFunction();
+    }
+}
+
--- GameEngine/Engine/Components/CustomEffectModel.cs
+++ GameEngine/Engine/Components/CustomEffectModel.cs
@@ -1,12 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
+using GameEngine.Engine.Core;
 
 namespace GameEngine.Engine.Components
 {
-    internal class CustomEffectModel
+    internal class CustomEffectModel : RenderComponent
     {
+        private string modelAssetName;
+        private string effectAssetName;
+        private string textureAssetName;
+
+        protected Model ModelAsset;
+        protected Effect EffectAsset;
+        protected Texture2D TextureAsset;
+
+        public Matrix World { get; set; }
+        private Matrix[] BoneTransforms;
+
+
+        public Vector3 DiffuseColor { get; set; } = Vector3.One;
+        public Vector3 AmbientColor { get; set; } = new Vector3(0.3f);
+        public Vector3 LightDirection { get; set; } = Vector3.Normalize(new Vector3(1, 1, 1));
+        public Vector3 LightColor { get; set; } = Vector3.One;
+        public bool TextureEnabled => TextureAsset != null;
+
+        public CustomEffectModel(string modelName, string effectName, Matrix transform)
+        {
+            modelAssetName = modelName;
+            effectAssetName = effectName;
+            World = transform;
+        }
+
+        public CustomEffectModel(string modelName, string effectName, string textureName, Matrix transform)
+        {
+            modelAssetName = modelName;
+            effectAssetName = effectName;
+            textureAssetName = textureName;
+            World = transform;
+        }
+
+        public virtual void LoadContent(ContentManager content)
+        {
+            ModelAsset = content.Load<Model>(modelAssetName);
+            BoneTransforms = new Matrix[ModelAsset.Bones.Count];
+            ModelAsset.CopyAbsoluteBoneTransformsTo(BoneTransforms);
+
+            if (!string.IsNullOrEmpty(textureAssetName))
+            {
+                TextureAsset = content.Load<Texture2D>(textureAssetName);
+            }
+
+            EffectAsset = content.Load<Effect>(effectAssetName);
+
+            foreach (ModelMesh mesh in ModelAsset.Meshes)
+                foreach (ModelMeshPart part in mesh.MeshParts)
+                {
+                    part.Effect = EffectAsset;
+                }
+        }
+
+        public virtual void Update() { }
+
+        public virtual void Draw(Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in ModelAsset.Meshes)
+            {
+                foreach (Effect effect in mesh.Effects)
+                {
+                    SetEffectParameter(effect, "World", BoneTransforms[mesh.ParentBone.Index] * World);
+                    SetEffectParameter(effect, "View", view);
+                    SetEffectParameter(effect, "Projection", projection);
+
+                    SetEffectParameters(effect);
+                }
+                mesh.Draw();
+            }
+        }
+
+        protected virtual void SetEffectParameters(Effect effect)
+        {
+            SetEffectParameter(effect, "DiffuseColor", DiffuseColor);
+            SetEffectParameter(effect, "AmbientColor", AmbientColor);
+            SetEffectParameter(effect, "LightDirection", LightDirection);
+            SetEffectParameter(effect, "LightColor", LightColor);
+            SetEffectParameter(effect, "TextureEnabled", TextureEnabled);
+
+            if (TextureEnabled)
+            {
+                SetEffectParameter(effect, "rocks", TextureAsset);
+            }
+        }
+
+        private void SetEffectParameter(Effect effect, string paramName, object value)
+        {
+            if (effect.Parameters[paramName] == null)
+                return;
+
+            if (value is Vector3 vec3)
+                effect.Parameters[paramName].SetValue(vec3);
+            else if (value is Matrix mat)
+                effect.Parameters[paramName].SetValue(mat);
+            else if (value is bool b)
+                effect.Parameters[paramName].SetValue(b);
+            else if (value is Texture2D tex)
+                effect.Parameters[paramName].SetValue(tex);
+            else if (value is float f)
+                effect.Parameters[paramName].SetValue(f);
+            else if (value is int i)
+                effect.Parameters[paramName].SetValue(i);
+        }
     }
 }
